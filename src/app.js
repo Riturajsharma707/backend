ac96424234867cfe5156b1799f029d23843c9146
Riturajsharma707 <riturajsharma54321@gmail.com>
2024-01-20 20:03:22 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 
-const app = express();
+const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -15,5 +15,17 @@ app.use(express.urlencoded({ extended: true, limit: '16kb' }))
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+
+// Routes import
+import userRouter from './routes/user.routes.js';
+
+
+//Routes declaration 
+
+app.use('/api/v1/users', userRouter)
+
+//http://localhost:8000/api/v1/users/register
+
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,38 @@
 // require('dotenv').config({path: './env'})
 
 import dotenv from 'dotenv';
-import { app } from './app.js'
-
-
-
-// import mongoose, { mongo } from "mongoose";
-// import { DB_NAME } from "./constants";
-
-import connectDB from "./db/index.js";
+import { app } from './app.js';
+import connectDB from './db/index.js';
 
 
 dotenv.config({
-    path: './.env'
+    path: '../.env'
 })
+
+
+
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8000, () => {
-            console.log(` Server is running at port : ${process.env.PORT}`);
+
+            console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
         })
     })
-    .catch((error) => {
-        console.log('Mongo DB connection failed !!', error);
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err);
     })
+
+
+// connectDB()
+//     .then(() => {
+//         app.listen(process.env.PORT || 8000, () => {
+//             console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+//         })
+//     })
+
+//     .catch((error) => {
+//         console.log('⚙️ Mongo DB connection failed !!', error);
+//     })
 
 
 
