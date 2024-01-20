c54ddd9f9118656ff361d3a76eac77da824a5ea0
Riturajsharma707 <riturajsharma54321@gmail.com>
2024-01-20 13:18:45 +0530

    MongoDB connected

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const DB_NAME = "videoyoutube"
+
+//Tow way we can connect database
+// 1. code in index file (when load the index)
+// 2. code in db and call in index.
---
+++ src/index.js
@@ -0,0 +1,37 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from 'dotenv'
+
+
+// import mongoose, { mongo } from "mongoose";
+// import { DB_NAME } from "./constants";
+
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: '../env'
+})
+connectDB()
+
+/*
+import express  from "express";
+
+const app= express();
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("ERROR ",(error)=>{
+            console.log("ERRR: ",error);
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`Applis listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("ERROR :", error);
+        throw error;
+    }
+})()
+
+*/
